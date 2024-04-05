932fed4709e1ca672604227a269e935b1ab832b4
Swaraj Jadhav <jadhavswaraj1920@gmail.com>
2024-04-05 10:35:16 +0530

    Updates

---
+++ Day-2/http.js
@@ -0,0 +1,18 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  console.log(req, "req");
+  if (req.method === "GET" && req.url === "/books") {
+    res.end("books");
+  } else if (req.method === "GET" && req.url === "/welcome") {
+    res.end("Welcome");
+  } else if (req.method === "GET" && req.url === "/bye") {
+    res.end("bye");
+  } else {
+    res.end("url undefined.");
+  }
+});
+
+server.listen(3001, () => {
+  console.log("Server listening on port 3001;");
+});
